e758a02374c5176f5eb7f24f05163b527827d707
wangdong289 <wD@890720>
2019-01-30 17:04:53 +0800

    aa

--- java/src/main/java/com/wd/dataStructure/LinkedList/SingleLinkedList.java
+++ java/src/main/java/com/wd/dataStructure/LinkedList/SingleLinkedList.java
@@ -1,12 +1,14 @@
 package com.wd.dataStructure.LinkedList;
 
+import java.util.Iterator;
+
 /**
  * 单链表类
  */
 
-public class SingleLinkedList<T> {
+public class SingleLinkedList<T> implements Iterator<T>{
 
-    private int size;
+    private int size = 0;
 
     //链表开始节点
     private Node<T> startNode;
@@ -16,6 +18,8 @@ public class SingleLinkedList<T> {
 
 
     private Node<T> temporaryNode;
+
+    private boolean isDisplay = false;
 
     public void add(T t) {
         Node<T> node = new Node<T>(t, null);
@@ -29,38 +33,123 @@ public class SingleLinkedList<T> {
         size++;
     }
 
-    public T remove() {
-        T t = null;
+    public void addHead(T t) {
+        Node<T> node = new Node<T>(t, null);
         if (startNode == null) {
-            return t;
+            startNode = node;
+            endNode = node;
+        } else {
+            node.setNextNode(startNode);
+            startNode = node;
+        }
+        size++;
+    }
+
+    public void add(Integer index,T t) {
+        if(index <0 || index>size){
+            throw new IndexOutOfBoundsException("index:"+index+";size"+size);
+        }
+        if(index == 0){
+            addHead(t);
+        }else if(index == size){
+            add(t);
+        }else{
+            Node<T> node = startNode;
+            int i = 0;
+            int j = index-1;
+            while (i != j) {
+                node = node.getNextNode();
+                i++;
+            }
+            Node<T> n = new Node<T>(t,node.getNextNode());
+            node.setNextNode(n);
+            size++;
+        }
+    }
+
+    public void remove() {
+        if(startNode == null){
+            return;
         }
         if(startNode.getNextNode() == null){
-            t = startNode.getT();
             startNode = null;
-            return t;
+            size =0;
+            return;
         }
-        Node<T> prevNode = null,
-                node = startNode;
-        while (node!=null){
-            prevNode = node;
+        int i=0;
+        int j = size - 2;
+        Node<T> node = startNode;
+        while (i!=j){
             node = node.getNextNode();
+            i++;
         }
-        t = prevNode.getT();
-        return t;
+        node.setNextNode(null);
+        size--;
+    }
+
+    public void removeHead() {
+        if(startNode == null){
+            return;
+        }
+        startNode = startNode.getNextNode();
+        size--;
+    }
+
+    public void remove(Integer index){
+        if(startNode == null){
+            return;
+        }
+        if(index <0 || index>size){
+            throw new IndexOutOfBoundsException("index:"+index+";size"+size);
+        }
+        if(index == 0){
+            removeHead();
+        }else if(index == size){
+            remove();
+        }else{
+            Node<T> node = startNode;
+            int i = 0;
+            while (i!=index){
+                node = node.getNextNode();
+                i++;
+            }
+            node.setT(node.getNextNode().getT());
+            node.setNextNode(node.getNextNode().getNextNode());
+            size--;
+        }
+    }
+
+    public T get(Integer index){
+        if(index <0 || index>size){
+            throw new IndexOutOfBoundsException("index:"+index+";size"+size);
+        }
+        if(index == 0){
+            return startNode.getT();
+        }else if(index == size){
+            return endNode.getT();
+        }else{
+            Node<T> node = startNode;
+            int i = 0;
+            while (i!=index){
+                node = node.getNextNode();
+                i++;
+            }
+            return node.getT();
+        }
     }
 
     public boolean hasNext() {
-//        if (temporaryNode == null) {
-//            temporaryNode = startNode;
-//        }
         if(startNode == null){
             return false;
-        }else {
-            if(temporaryNode == null){
-                temporaryNode = startNode;
-            }
         }
-        return temporaryNode != null;
+        if(temporaryNode == null && !isDisplay){//开始遍历
+            isDisplay = true;
+            temporaryNode = startNode;
+        }else if(temporaryNode == null && isDisplay){//遍历结束
+            isDisplay = false;
+            return false;
+        }
+        return true;
     }
 
     public T next() {
@@ -75,13 +164,22 @@ public class SingleLinkedList<T> {
     public void display() {
         if (startNode != null) {
             Node<T> node = startNode;
+            int i=0;
             while (node != null) {
                 System.out.println(node.getT());
+//                System.out.println("i="+(++i));
                 node = node.getNextNode();
             }
         }
     }
 
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 
     /**
      * 链表节点类
--- java/src/main/java/com/wd/dataStructure/LinkedList/Test.java
+++ java/src/main/java/com/wd/dataStructure/LinkedList/Test.java
@@ -1,10 +1,8 @@
 package com.wd.dataStructure.LinkedList;
 
-import java.util.Iterator;
-
 public class Test {
 
-    public static void main(String[] arg0){
+    public static void main(String[] arg0) {
         SingleLinkedList<Integer> singleLinkedList = new SingleLinkedList<Integer>();
         singleLinkedList.add(1);
         singleLinkedList.add(2);
@@ -12,14 +10,10 @@ public class Test {
         singleLinkedList.add(4);
         singleLinkedList.add(5);
         singleLinkedList.add(6);
+        singleLinkedList.add(3,7);
         singleLinkedList.display();
-        System.out.println("***********************************");
-        while (singleLinkedList.hasNext()){
-            System.out.println(singleLinkedList.next());
-        }
-        System.out.println("***********************************");
-        singleLinkedList.remove();
-        singleLinkedList.display();
-        System.out.println(singleLinkedList);
+//        singleLinkedList.remove();
+//        System.out.println("***********************************");
+//        singleLinkedList.display();
     }
 }
